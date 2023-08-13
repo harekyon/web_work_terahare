d7ba669897e9e0e20d7ff9d22de29aa8e0879fd9
harekyon <harekyon@gmail.com>
2023-08-14 01:54:50 +0900

    threejsのテンプレをクローンし、最低限の状態に

--- app/page.js
+++ app/page.js
@@ -43,9 +43,6 @@ export default function Home() {
       0.1,
       1000
     );
-    // camera.position.x = -30;
-    // camera.position.y = 40;
-    // camera.position.z = 30;
     camera.position.x = -30;
     camera.position.y = 60;
     camera.position.z = 10;
@@ -130,63 +127,19 @@ export default function Home() {
     floor.name = "floor";
     floor.castShadow = true;
     floor.receiveShadow = true;
-    let leftWall = new THREE.Mesh(planeGeometry, wallMaterials);
-    leftWall.receiveShadow = true;
-    leftWall.rotation.z = 0.5 * Math.PI;
-    leftWall.position.x = 0;
-    leftWall.position.y = 21;
-    leftWall.position.z = -21;
-    leftWall.name = "leftWall";
-    let rightWall = new THREE.Mesh(planeGeometry, wallMaterials);
-    rightWall.receiveShadow = true;
-    rightWall.rotation.y = -0.5 * Math.PI;
-    rightWall.position.x = 21;
-    rightWall.position.y = 21;
-    rightWall.position.z = 0;
-    rightWall.name = "rightWall";
-    let frontWall = new THREE.Mesh(planeGeometry, wallMaterials);
-    frontWall.receiveShadow = true;
-    frontWall.rotation.y = -Math.PI;
-    frontWall.position.x = 0;
-    frontWall.position.y = 21;
-    frontWall.position.z = 21;
-    frontWall.name = "frontWall";
     scene.add(floor);
-    scene.add(leftWall);
-    scene.add(rightWall);
-    // scene.add(frontWall);
-
     intersectObjects.push(floor);
-    intersectObjects.push(leftWall);
-    intersectObjects.push(rightWall);
 
     let plane = textToTextureConvertReturnMesh();
     plane.position.y = 5;
     plane.position.x = 10;
     plane.name = "plane";
-    let plane2 = textToTextureConvertReturnMesh();
-    plane2.position.y = 10;
-    plane2.position.x = -15;
-    plane2.scale.set(2, 2, 2);
-    plane2.name = "plane";
-    // scene.add(plane);
-    // scene.add(plane2);
     let selectObjectText = textToTextureConvertReturnMesh();
     selectObjectText.position.y = 15;
     selectObjectText.position.x = 0;
     selectObjectText.scale.set(3, 3, 3);
     selectObjectText.name = "plane";
     // scene.add(selectObjectText);
-
-    const displayObj = exportGltf({
-      glbPath: `${
-        mode === "PRODUCT" ? "/threePractice/display.glb" : "/display.glb"
-      }`,
-    });
-    displayObj.rotation.y = (Math.PI / 180) * -90;
-    displayObj.position.set(20, 10, 0);
-    displayObj.receiveShadow = true;
-    scene.add(displayObj);
 
     // const tableObj = exportGltf({ glbPath: "/table.glb" });
     const tableObj = exportGltf({
@@ -216,53 +169,6 @@ export default function Home() {
     artGlassObj.receiveShadow = true;
     scene.add(artGlassObj);
 
-    // const pypeObj = exportGltf({ glbPath: "/pype.glb" });
-    const pypeObj = exportGltf({
-      glbPath: mode === "PRODUCT" ? "/threePractice/pype.glb" : "/pype.glb",
-    });
-    pypeObj.rotation.y = calcRadian(89);
-    pypeObj.position.y = 0.2;
-    pypeObj.position.set(-12, 0.2, -18);
-    pypeObj.receiveShadow = true;
-    scene.add(pypeObj);
-
-    // const rackObj = exportGltf({ glbPath: "/rack.glb" });
-    const rackObj = exportGltf({
-      glbPath: mode === "PRODUCT" ? "/threePractice/rack.glb" : "/rack.glb",
-    });
-    rackObj.rotation.y = calcRadian(89);
-    rackObj.position.y = 0.2;
-    rackObj.position.set(14, 0.2, -19);
-    rackObj.receiveShadow = true;
-    scene.add(rackObj);
-
-    // const awardWallObj = exportGltf({
-    //   glbPath: "/award.glb",
-    // });
-    const awardWallObj = exportGltf({
-      glbPath: mode === "PRODUCT" ? "/threePractice/award.glb" : "/award.glb",
-    });
-    awardWallObj.rotation.y = calcRadian(90);
-    awardWallObj.position.set(0, -0.4, 23.5);
-    awardWallObj.scale.y = 1.01;
-    awardWallObj.receiveShadow = true;
-    scene.add(awardWallObj);
-    // const awardWallGlassObj = exportGltf({
-    //   glbPath: "/award_glass.glb",
-    //   transparent: true,
-    // });
-    const awardWallGlassObj = exportGltf({
-      glbPath:
-        mode === "PRODUCT"
-          ? "/threePractice/award_glass.glb"
-          : "/award_glass.glb",
-      transparent: true,
-    });
-    awardWallGlassObj.rotation.y = calcRadian(90);
-    awardWallGlassObj.position.set(0, 0, 23.5);
-    awardWallGlassObj.receiveShadow = true;
-    // scene.add(awardWallGlassObj);
-    // const book1 = exportGltf({ glbPath: "/book.glb" });
     const book1 = exportGltf({
       glbPath: mode === "PRODUCT" ? "/threePractice/book.glb" : "/book.glb",
     });
@@ -357,15 +263,6 @@ export default function Home() {
     let ambientLight = new THREE.AmbientLight(0x444444);
     scene.add(ambientLight);
 
-    let outSideLight = new THREE.PointLight(0xffffff);
-    outSideLight.penumbra = 0.5;
-    outSideLight.angle = 0.5;
-    outSideLight.intensity = 0.2;
-    outSideLight.castShadow = true;
-    scene.add(outSideLight);
-    let outSideLightHelper = new THREE.PointLightHelper(outSideLight, 5);
-    // scene.add(outSideLightHelper);
-
     let pointLights = [];
     let pointLightsHelpers = [];
     for (let n = 0; n < 6; n++) {
@@ -395,9 +292,6 @@ export default function Home() {
       },
       myDropDowns: "select1",
       myColor: 0xff0000,
-      outSideLightPosX: 0,
-      outSideLightPosY: 27,
-      outSideLightPosZ: 32,
 
       RoomLight0Color: 0xfebe48,
       RoomLight1Color: 0xffc966,
@@ -475,83 +369,10 @@ export default function Home() {
         0.1
       );
     }
-    let outSideLightFolder = gui.addFolder("outSideLight");
-    outSideLightFolder.add(controls, "outSideLightPosX", -50, 50, 1);
-    outSideLightFolder.add(controls, "outSideLightPosY", -50, 50, 1);
-    outSideLightFolder.add(controls, "outSideLightPosZ", -50, 50, 1);
-
-    // gui.add();
     function capitalize(str) {
       if (typeof str !== "string" || !str) return str;
       return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
     }
-
-    // const bookPosition = book1.getWorldPosition(new THREE.Vector3());
-    // let projection = bookPosition.project(camera);
-    // let sx = (window.innerWidth / 2) * (projection.x + 1.0);
-    // let sy = (window.innerHeight / 2) * (-projection.x + 1.0);
-    // console.log(`sx:${sx}, sy:${sy}`);
-
-    // vector.project(camera);
-
-    // vector.x = Math.round(
-    //   (0.5 + vector.x / 2) * (canvas.width / window.devicePixelRatio)
-    // );
-    // vector.y = Math.round(
-    //   (0.5 - vector.y / 2) * (canvas.height / window.devicePixelRatio)
-    // );
-
-    // const annotation = document.querySelector(".annotation");
-    // annotation.style.top = `${vector.y}px`;
-    // annotation.style.left = `${vector.x}px`;
-
-    // const y = 32;
-    // const radius = 30;
-    // const startAngle = 30;
-    // const endAngle = Math.PI * 2;
-
-    // console.log(ctx);
-    // ctx.fillStyle = "rgb(0, 0, 0)";
-    // ctx.beginPath();
-    // ctx.arc(x, y, radius, startAngle, endAngle);
-    // ctx.fill();
-
-    // ctx.strokeStyle = "rgb(255, 255, 255)";
-    // ctx.lineWidth = 3;
-    // ctx.beginPath();
-    // ctx.arc(x, y, radius, startAngle, endAngle);
-    // ctx.stroke();
-
-    // ctx.fillStyle = "rgb(255, 255, 255)";
-    // ctx.font = "32px sans-serif";
-    // ctx.textAlign = "center";
-    // ctx.textBaseline = "middle";
-    // ctx.fillText("1", x, y);
-
-    // const numberTexture = new THREE.CanvasTexture(
-    //   document.querySelector("#number")
-    // );
-
-    // const spriteMaterial = new THREE.SpriteMaterial({
-    //   map: numberTexture,
-    //   alphaTest: 0.5,
-    //   transparent: true,
-    //   depthTest: false,
-    //   depthWrite: false,
-    // });
-
-    // let sprite = new THREE.Sprite(spriteMaterial);
-    // sprite.position.set(250, 250, 250);
-    // sprite.scale.set(35, 35, 1);
-
-    // scene.add(sprite);
-
-    // const meshDistance = camera.position.distanceTo(mesh.position);
-    // const spriteDistance = camera.position.distanceTo(sprite.position);
-    // spriteBehindObject = spriteDistance > meshDistance;
-
-    // sprite.material.opacity = spriteBehindObject ? 0.25 : 1;
-    // annotation.style.opacity = spriteBehindObject ? 0.25 : 1;
 
     document.getElementById("WebGL-output").appendChild(renderer.domElement);
     render();
@@ -559,21 +380,10 @@ export default function Home() {
     function render() {
       stats.update();
       orbitControl.instance.update(orbitControler.delta);
-      // console.log(cameraControl);
       plane.update(Date.now());
       plane.quaternion.copy(camera.quaternion);
-      plane2.update(Date.now());
-      plane2.quaternion.copy(camera.quaternion);
       selectObjectText.update(sel.current);
       selectObjectText.quaternion.copy(camera.quaternion);
-      // const bookPosition = book1.getWorldPosition(new THREE.Vector3());
-      // const projection = bookPosition.project(camera);
-      // const sx = (window.innerWidth / 2) * (projection.x + 1.0);
-      // const sy = (window.innerHeight / 2) * (-projection.y + 1.0);
-      // // console.log(projection);
-      // console.log(`sx:${sx}, sy:${sy}`);
-      // const annotation = document.querySelector(".annotation");
-      // annotation.style.transform = `translate(${sx}px, ${sy}px)`;
       simpleAnnotation({
         mesh: book1,
         camera: camera,
@@ -590,8 +400,6 @@ export default function Home() {
         addTextTitle: "BLUE BOOK",
         addTextAbs: "クリックでパーティクルを表示する",
       });
-      // annotation.style.top = `${sy}px`;
-      // annotation.style.left = `${sx}px`;
 
       for (let n = 0; n < 6; n++) {
         pointLights[n].color.setHex(controls[eval(`"RoomLight${n}Color"`)]);
@@ -599,22 +407,8 @@ export default function Home() {
         pointLights[n].position.x = controls[eval(`"RoomLight${n}PosX"`)];
         pointLights[n].position.y = controls[eval(`"RoomLight${n}PosY"`)];
         pointLights[n].position.z = controls[eval(`"RoomLight${n}PosZ"`)];
-        // pointLightsHelpers[n].position.x = pointLights[n].position.x;
-        // pointLightsHelpers[n].position.y = pointLights[n].position.y;
-        // pointLightsHelpers[n].position.z = pointLights[n].position.z;
         scene.add(pointLights[n]);
-        // scene.add(pointLightsHelpers[n]);
       }
-      outSideLight.position.set(
-        controls.outSideLightPosX,
-        controls.outSideLightPosY,
-        controls.outSideLightPosZ
-      );
-      outSideLight.rotation.set(
-        calcRadian(controls.outSideLightRotX),
-        calcRadian(controls.outSideLightRotY),
-        calcRadian(controls.outSideLightRotZ)
-      );
 
       requestAnimationFrame(render);
       renderer.render(scene, camera);
@@ -660,12 +454,10 @@ export default function Home() {
           intersects[0].object.log("click");
         }
       }
-      console.log("run");
     }
 
     window.addEventListener("mousemove", returnHoverObj);
     function returnHoverObj(e) {
-      // console.log(book1.name);
       let mouseX =
         ((e.offsetX - window.innerWidth / 2) / window.innerWidth) * 2;
       let mouseY =
@@ -677,21 +469,10 @@ export default function Home() {
         pos.sub(camera.position).normalize()
       );
       objs.current = ray.intersectObjects(scene.children);
-
-      let objsResult = objs.current.map((obj) => {
-        // console.log(obj.object);
-        // if (obj.object.name === "redbook") {
-        //   console.log(objsResult);
-        // }
-      });
     }
   }, []);
   return (
     <>
-      {/* <div className="annotation" id="annotation1">
-        <p>Book</p>
-        <p>three.js practices.</p>
-      </div> */}
       <div id="Stats-output"></div>
       <div id="WebGL-output"></div>
     </>
